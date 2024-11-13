57dcda483da3a2146601f044e0f14058f334f6e6
taskat <tamas.taska@gmx.com>
2024-11-13 21:49:50 +0000

    Simplified year template

--- internal/years/2023/register.go
+++ internal/years/2023/register.go
@@ -6,10 +6,7 @@ import (
 	_ "taskat/aoc/internal/years/2023/imports"
 )
 
-// year is the year of the solvers
-const year = 2023
-
 // init register the year's collection of solvers
 func init() {
-	years.AddYear(year, days.Current)
+	years.AddYear(days.Year, days.Current)
 }
