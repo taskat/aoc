a65116eef22643a70325034160be7ed9fb3513f5
taskat <tamas.taska@gmx.com>
2024-12-01 22:03:07 +0000

    Added basic operations to int utils

--- pkg/utils/intutils/operations.go
+++ pkg/utils/intutils/operations.go
@@ -9,3 +9,32 @@ func Abs[INT types.SignedInteger](number INT) INT {
 	}
 	return number
 }
+
+// Equals returns true if the two numbers are equal and false otherwise
+func Equals[INT types.Integer](a, b INT) bool {
+	return a == b
+}
+
+// Diff returns the difference between the two numbers
+// as Diff(a, b) = a - b
+func Diff[INT types.SignedInteger](a, b INT) INT {
+	return a - b
+}
+
+// Product returns the product of the two numbers
+// as Product(a, b) = a * b
+func Product[INT types.SignedInteger](a, b INT) INT {
+	return a * b
+}
+
+// Quotient returns the quotient of the two numbers
+// as Quotient(a, b) = a / b as integer
+func Quotient[INT types.SignedInteger](a, b INT) INT {
+	return a / b
+}
+
+// Sum returns the sum of the two numbers
+// as Sum(a, b) = a + b
+func Sum[INT types.SignedInteger](a, b INT) INT {
+	return a + b
+}
--- pkg/utils/intutils/operations_test.go
+++ pkg/utils/intutils/operations_test.go
@@ -23,3 +23,122 @@ func TestAbc(t *testing.T) {
 		})
 	}
 }
+
+func TestEquals(t *testing.T) {
+	testCases := []struct {
+		testName      string
+		a             int
+		b             int
+		expectedValue bool
+	}{
+		{"Positive numbers", 5, 3, false},
+		{"Negative numbers", -5, -3, false},
+		{"Positive and negative numbers", 5, -3, false},
+		{"Negative and positive numbers", -5, 3, false},
+		{"Zero and positive numbers", 0, 3, false},
+		{"Positive and zero numbers", 5, 0, false},
+		{"Zero and negative numbers", 0, -3, false},
+		{"Negative and zero numbers", -5, 0, false},
+		{"Equal numbers", 5, 5, true},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			result := Equals(tc.a, tc.b)
+			assert.Equal(t, tc.expectedValue, result)
+		})
+	}
+}
+
+func TestDiff(t *testing.T) {
+	testCases := []struct {
+		testName      string
+		a             int
+		b             int
+		expectedValue int
+	}{
+		{"Positive numbers", 5, 3, 2},
+		{"Negative numbers", -5, -3, -2},
+		{"Positive and negative numbers", 5, -3, 8},
+		{"Negative and positive numbers", -5, 3, -8},
+		{"Zero and positive numbers", 0, 3, -3},
+		{"Positive and zero numbers", 5, 0, 5},
+		{"Zero and negative numbers", 0, -3, 3},
+		{"Negative and zero numbers", -5, 0, -5},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			result := Diff(tc.a, tc.b)
+			assert.Equal(t, tc.expectedValue, result)
+		})
+	}
+}
+
+func TestProduct(t *testing.T) {
+	testCases := []struct {
+		testName      string
+		a             int
+		b             int
+		expectedValue int
+	}{
+		{"Positive numbers", 5, 3, 15},
+		{"Negative numbers", -5, -3, 15},
+		{"Positive and negative numbers", 5, -3, -15},
+		{"Negative and positive numbers", -5, 3, -15},
+		{"Zero and positive numbers", 0, 3, 0},
+		{"Positive and zero numbers", 5, 0, 0},
+		{"Zero and negative numbers", 0, -3, 0},
+		{"Negative and zero numbers", -5, 0, 0},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			result := Product(tc.a, tc.b)
+			assert.Equal(t, tc.expectedValue, result)
+		})
+	}
+}
+
+func TestQuotient(t *testing.T) {
+	testCases := []struct {
+		testName      string
+		a             int
+		b             int
+		expectedValue int
+	}{
+		{"Positive numbers", 5, 3, 1},
+		{"Negative numbers", -5, -3, 1},
+		{"Positive and negative numbers", 5, -3, -1},
+		{"Negative and positive numbers", -5, 3, -1},
+		{"Zero and positive numbers", 0, 3, 0},
+		{"Zero and negative numbers", 0, -3, 0},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			result := Quotient(tc.a, tc.b)
+			assert.Equal(t, tc.expectedValue, result)
+		})
+	}
+}
+
+func TestSum(t *testing.T) {
+	testCases := []struct {
+		testName      string
+		a             int
+		b             int
+		expectedValue int
+	}{
+		{"Positive numbers", 5, 3, 8},
+		{"Negative numbers", -5, -3, -8},
+		{"Positive and negative numbers", 5, -3, 2},
+		{"Negative and positive numbers", -5, 3, -2},
+		{"Zero and positive numbers", 0, 3, 3},
+		{"Positive and zero numbers", 5, 0, 5},
+		{"Zero and negative numbers", 0, -3, -3},
+		{"Negative and zero numbers", -5, 0, -5},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			result := Sum(tc.a, tc.b)
+			assert.Equal(t, tc.expectedValue, result)
+		})
+	}
+}
