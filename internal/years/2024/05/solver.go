d9c7b465b04e399a2faee90045ac9f0e9bfc4cfd
taskat <tamas.taska@gmx.com>
2024-12-05 23:36:42 +0000

    Added solution for 2024 day 5 part 2

--- internal/years/2024/05/solver.go
+++ internal/years/2024/05/solver.go
@@ -50,6 +50,26 @@ func parseManual(s string) manual {
 	return slices.Map(pages, stringutils.Atoi)
 }
 
+func (m manual) fixOrder(rules []rule) {
+	if m.isCorrect(rules) {
+		return
+	}
+	for i, page := range m {
+		for _, r := range rules {
+			if r.after != page {
+				continue
+			}
+			for j := i + 1; j < len(m); j++ {
+				if m[j] == r.before {
+					m[i], m[j] = m[j], m[i]
+					m.fixOrder(rules)
+					return
+				}
+			}
+		}
+	}
+}
+
 func (m manual) isCorrect(rules []rule) bool {
 	for i, page := range m {
 		for _, r := range rules {
@@ -77,5 +97,10 @@ func (s *Solver) SolvePart1(lines []string) string {
 
 // SolvePart2 solves part 2 of the puzzle
 func (s *Solver) SolvePart2(lines []string) string {
-	return ""
+	rules, manuals := s.parse(lines)
+	manuals = slices.Filter(manuals, func(m manual) bool { return !m.isCorrect(rules) })
+	slices.ForEach(manuals, func(m manual) { m.fixOrder(rules) })
+	middlePages := slices.Map(manuals, func(m manual) int { return m[len(m)/2] })
+	sum := slices.Sum(middlePages)
+	return fmt.Sprintf("%d", sum)
 }
--- internal/years/2024/05/solver_test.go
+++ internal/years/2024/05/solver_test.go
@@ -57,8 +57,8 @@ func Test_Day_05_Part2(t *testing.T) {
 		expectedValue string
 		hyperParams   []any
 	}{
-		{"Test 1", config.TestInput(1), "", nil},
-		{"Real", config.RealInput{}, "", nil},
+		{"Test 1", config.TestInput(1), "123", nil},
+		{"Real", config.RealInput{}, "4130", nil},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
