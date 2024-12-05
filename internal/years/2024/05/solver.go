33d8718753a539ba3565a02114d56455d3d2df4a
taskat <tamas.taska@gmx.com>
2024-12-05 23:53:27 +0000

    Refactored 2024 day 5 solutions

--- internal/years/2024/05/solver.go
+++ internal/years/2024/05/solver.go
@@ -32,65 +32,73 @@ func (s *Solver) parse(lines []string) ([]rule, []manual) {
 	return rules, manuals
 }
 
+// rule represents a page ordering rule. The page with number `before` must be
+// before the page with number `after`
 type rule struct {
 	before int
 	after  int
 }
 
+// parseRule parses a rule from a string with the format "before|after"
 func parseRule(s string) rule {
 	before, after := 0, 0
 	_, _ = fmt.Sscanf(s, "%d|%d", &before, &after)
 	return rule{before: before, after: after}
 }
 
+// manual represents a manual with the order of the pages
 type manual []int
 
+// parseManual parses a manual from a string with the format "page1,page2,..."
 func parseManual(s string) manual {
 	pages := strings.Split(s, ",")
 	return slices.Map(pages, stringutils.Atoi)
 }
 
+// findProblematicPages returns the indices of two page numbers that are in
+// the wrong order according to the given rules. If all pages are in the correct
+// order, it returns -1, -1
+func (m manual) findProblematicPages(rules []rule) (int, int) {
+	for i, page := range m {
+		for _, r := range rules {
+			if r.after != page {
+				continue
+			}
+			for j := i + 1; j < len(m); j++ {
+				if m[j] == r.before {
+					return i, j
+				}
+			}
+		}
+	}
+	return -1, -1
+}
+
+// fixOrder tries to fix the order of the pages in the manual according to the
+// given rules. It does not return anything, it just modifies the manual in
+// place. If the manual is already correct, it does nothing.
 func (m manual) fixOrder(rules []rule) {
 	if m.isCorrect(rules) {
 		return
 	}
-	for i, page := range m {
-		for _, r := range rules {
-			if r.after != page {
-				continue
-			}
-			for j := i + 1; j < len(m); j++ {
-				if m[j] == r.before {
-					m[i], m[j] = m[j], m[i]
-					m.fixOrder(rules)
-					return
-				}
-			}
-		}
-	}
+	before, after := m.findProblematicPages(rules)
+	slices.Swap(m, before, after)
+	m.fixOrder(rules)
+	return
 }
 
+// isCorrect returns true if the manual is in the correct order according to the
+// given rules
 func (m manual) isCorrect(rules []rule) bool {
-	for i, page := range m {
-		for _, r := range rules {
-			if r.after != page {
-				continue
-			}
-			for j := i + 1; j < len(m); j++ {
-				if m[j] == r.before {
-					return false
-				}
-			}
-		}
-	}
-	return true
+	before, after := m.findProblematicPages(rules)
+	return before == -1 && after == -1
 }
 
 // SolvePart1 solves part 1 of the puzzle
 func (s *Solver) SolvePart1(lines []string) string {
 	rules, manuals := s.parse(lines)
 	manuals = slices.Filter(manuals, func(m manual) bool { return m.isCorrect(rules) })
-	middlePages := slices.Map(manuals, func(m manual) int { return m[len(m)/2] })
+	middlePages := slices.Map(manuals, slices.Middle)
 	sum := slices.Sum(middlePages)
 	return fmt.Sprintf("%d", sum)
 }
@@ -100,7 +108,7 @@ func (s *Solver) SolvePart2(lines []string) string {
 	rules, manuals := s.parse(lines)
 	manuals = slices.Filter(manuals, func(m manual) bool { return !m.isCorrect(rules) })
 	slices.ForEach(manuals, func(m manual) { m.fixOrder(rules) })
-	middlePages := slices.Map(manuals, func(m manual) int { return m[len(m)/2] })
+	middlePages := slices.Map(manuals, slices.Middle)
 	sum := slices.Sum(middlePages)
 	return fmt.Sprintf("%d", sum)
 }
