dfe7462f916d5657b8b4d95c2c178c05424fc1b5
taskat <tamas.taska@gmx.com>
2024-12-06 19:11:47 +0000

    Added solution for 2024 day 6 part 2

--- internal/years/2024/06/solver.go
+++ internal/years/2024/06/solver.go
@@ -72,12 +72,51 @@ func parseField(r rune) field {
 
 type lab [][]field
 
+func (l lab) copy() lab {
+	result := make(lab, len(l))
+	for i, row := range l {
+		result[i] = make([]field, len(row))
+		copy(result[i], row)
+	}
+	return result
+}
+
 func (l lab) isEmpty(c coordinate) bool {
 	return l[c.y][c.x] != obstruction
 }
 
 func (l lab) isOutOfBounds(c coordinate) bool {
 	return c.x < 0 || c.x >= len(l[0]) || c.y < 0 || c.y >= len(l)
+}
+
+func (l lab) leadsToCircle(c coordinate, d direction) bool {
+	visitedFields := make(map[coordinate][]direction)
+	fakeGuard := guard{
+		position: c.copy(),
+		facing:   d,
+	}
+	visitedFields[c] = []direction{d}
+	fakeLab := l.copy()
+	for fakeGuard.move(fakeLab) {
+		dir, ok := visitedFields[fakeGuard.position]
+		if ok && slices.Contains(dir, fakeGuard.facing) {
+			return true
+		}
+		visitedFields[fakeGuard.position] = append(dir, fakeGuard.facing)
+	}
+	return false
+}
+
+// String implements the fmt.Stringer interface
+func (l lab) String() string {
+	result := ""
+	for _, row := range l {
+		for _, f := range row {
+			result += string(f)
+		}
+		result += "\n"
+	}
+	return result
 }
 
 func (l lab) visit(c coordinate) {
@@ -91,20 +130,28 @@ type guard struct {
 
 func (g *guard) move(l lab) bool {
 	l.visit(g.position)
-	next := g.position.move_new(g.facing)
+	next := g.next()
 	if l.isOutOfBounds(next) {
 		return false
 	}
 	if l.isEmpty(next) {
 		g.position = next
 	} else {
-		g.facing = (g.facing + 1) % 4
+		g.facing = g.facing.turnRight()
 	}
 	return true
 }
 
+func (g guard) next() coordinate {
+	return g.position.move_new(g.facing)
+}
+
 type coordinate struct {
 	x, y int
+}
+
+func (c coordinate) copy() coordinate {
+	return c
 }
 
 func (c coordinate) move_new(d direction) coordinate {
@@ -130,6 +177,10 @@ const (
 	left
 )
 
+func (d direction) turnRight() direction {
+	return (d + 1) % 4
+}
+
 // SolvePart1 solves part 1 of the puzzle
 func (s *Solver) SolvePart1(lines []string) string {
 	lab, guard := s.parse(lines)
@@ -144,5 +195,27 @@ func (s *Solver) SolvePart1(lines []string) string {
 
 // SolvePart2 solves part 2 of the puzzle
 func (s *Solver) SolvePart2(lines []string) string {
-	return ""
+	lab, guard := s.parse(lines)
+	start := guard.position.copy()
+	count := 0
+	for {
+		next := guard.next()
+		if lab.isOutOfBounds(next) {
+			break
+		}
+		if next == start || lab[next.y][next.x] == visited || lab[next.y][next.x] == obstruction {
+			guard.move(lab)
+			continue
+		}
+		old := lab[next.y][next.x]
+		lab[next.y][next.x] = obstruction
+		if lab.leadsToCircle(guard.position, guard.facing) {
+			count++
+		}
+		lab[next.y][next.x] = old
+		if !guard.move(lab) {
+			break
+		}
+	}
+	return fmt.Sprint(count)
 }
--- internal/years/2024/06/solver_test.go
+++ internal/years/2024/06/solver_test.go
@@ -57,8 +57,8 @@ func Test_Day_06_Part2(t *testing.T) {
 		expectedValue string
 		hyperParams   []any
 	}{
-		{"Test 1", config.TestInput(1), "", nil},
-		{"Real", config.RealInput{}, "", nil},
+		{"Test 1", config.TestInput(1), "6", nil},
+		{"Real", config.RealInput{}, "1740", nil},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
