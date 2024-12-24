17cef3d834a07dcefa5516a847673b774206c278
taskat <tamas.taska@gmx.com>
2024-12-24 18:27:19 +0000

    Half solution for 2024 day 24 part 2

--- internal/years/2024/24/solver.go
+++ internal/years/2024/24/solver.go
@@ -2,6 +2,7 @@ package day24
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/taskat/aoc/internal/years/2024/days"
@@ -45,6 +46,8 @@ type wire string
 type gate interface {
 	canExecute(wires map[wire]bool) bool
 	execute(wires map[wire]bool) bool
+	getInput1() wire
+	getInput2() wire
 	getOutput() wire
 }
 
@@ -80,6 +83,18 @@ func (g *baseGate) canExecute(wires map[wire]bool) bool {
 	_, ok1 := wires[g.input1]
 	_, ok2 := wires[g.input2]
 	return ok1 && ok2
+}
+
+func (g *baseGate) execute(wires map[wire]bool) bool {
+	panic("Not implemented")
+}
+
+func (g *baseGate) getInput1() wire {
+	return g.input1
+}
+
+func (g *baseGate) getInput2() wire {
+	return g.input2
 }
 
 func (g *baseGate) getOutput() wire {
@@ -150,7 +165,145 @@ func (s *Solver) SolvePart1(lines []string) string {
 	return fmt.Sprint(number)
 }
 
+func findWire[T gate](gates []gate, input1, input2 wire) wire {
+	for _, g := range gates {
+		gate, ok := g.(T)
+		if !ok {
+			continue
+		}
+		if (gate.getInput1() == input1 && gate.getInput2() == input2) || (gate.getInput1() == input2 && gate.getInput2() == input1) {
+			return gate.getOutput()
+		}
+	}
+	return ""
+}
+
+func findGateByInput[T gate](gates []gate, input wire) (T, bool) {
+	for _, g := range gates {
+		gate, ok := g.(T)
+		if !ok {
+			continue
+		}
+		if gate.getInput1() == input || gate.getInput2() == input {
+			return gate, true
+		}
+	}
+	var zero T
+	return zero, false
+}
+
+func finGateByOutput[T gate](gates []gate, output wire) (T, bool) {
+	for _, g := range gates {
+		gate, ok := g.(T)
+		if !ok {
+			continue
+		}
+		if g.getOutput() == output {
+			return gate, true
+		}
+	}
+	var zero T
+	return zero, false
+}
+
+func check0Bit(gates []gate) (wire, []wire) {
+	carryWire := findWire[*andGate](gates, "x00", "y00")
+	resultWire := findWire[*xorGate](gates, "x00", "y00")
+	if resultWire != "z00" {
+		return carryWire, []wire{resultWire, wire("z00")}
+	}
+	return carryWire, nil
+}
+
+func checkNthBit(gates []gate, n int, carry wire) (wire, []wire) {
+	wrongs := make([]wire, 0)
+	xWire := wire(fmt.Sprintf("x%02d", n))
+	yWire := wire(fmt.Sprintf("y%02d", n))
+	zWire := wire(fmt.Sprintf("z%02d", n))
+	bitResult := findWire[*xorGate](gates, xWire, yWire)
+	resultWire := findWire[*xorGate](gates, bitResult, carry)
+	zGate, ok := finGateByOutput[*xorGate](gates, zWire)
+	if !ok {
+		wrongs = append(wrongs, zWire, resultWire)
+	} else {
+		switch resultWire {
+		case "":
+			// carry or bitrsult is wrong
+			if zGate.getInput1() == carry {
+				wrongs = append(wrongs, zGate.getInput2(), bitResult)
+				bitResult = zGate.getInput2()
+			} else if zGate.getInput2() == carry {
+				wrongs = append(wrongs, zGate.getInput1(), bitResult)
+				bitResult = zGate.getInput1()
+			} else if zGate.getInput1() == bitResult {
+				wrongs = append(wrongs, zGate.getInput2(), carry)
+				carry = zGate.getInput2()
+			} else if zGate.getInput2() == bitResult {
+				wrongs = append(wrongs, zGate.getInput1(), carry)
+				carry = zGate.getInput1()
+			} else {
+				// both wrong
+				wrongs = append(wrongs, zGate.getInput1(), zGate.getInput2(), carry, bitResult)
+			}
+		case zWire:
+			// carry and bitresult is ok
+		default:
+			// output is wrong
+			wrongs = append(wrongs, resultWire, zWire)
+		}
+	}
+	resultCarry := findWire[*andGate](gates, carry, bitResult)
+	bitCarry := findWire[*xorGate](gates, xWire, yWire)
+	newCarry := findWire[*orGate](gates, bitCarry, resultCarry)
+	if newCarry != "" {
+		return newCarry, wrongs
+	}
+	carryGate, ok := findGateByInput[*andGate](gates, bitCarry)
+	if ok {
+		if carryGate.getInput1() == bitCarry {
+			wrongs = append(wrongs, carryGate.getInput2(), resultCarry)
+		} else {
+			wrongs = append(wrongs, carryGate.getInput1(), resultCarry)
+		}
+		return carryGate.output, wrongs
+	}
+	carryGate, ok = findGateByInput[*andGate](gates, resultCarry)
+	if ok {
+		if carryGate.getInput1() == resultCarry {
+			wrongs = append(wrongs, carryGate.getInput2(), bitCarry)
+		} else {
+			wrongs = append(wrongs, carryGate.getInput1(), bitCarry)
+		}
+		return carryGate.output, wrongs
+	}
+	fmt.Println(n)
+	panic("unrecoverable")
+}
+
+func checkLastBit(n int, carry wire) []wire {
+	zWire := wire(fmt.Sprintf("z%02d", n))
+	if carry != zWire {
+		return []wire{zWire, carry}
+	}
+	return nil
+}
+
 // SolvePart2 solves part 2 of the puzzle
 func (s *Solver) SolvePart2(lines []string) string {
+	_, gates := s.parse(lines)
+	zWires := slices.Filter(gates, func(g gate) bool { return strings.HasPrefix(string(g.getOutput()), "z") })
+	wireNames := slices.Map(zWires, func(g gate) string { return string(g.getOutput()) })
+	sort.Strings(wireNames)
+	maxZ := wireNames[len(wireNames)-1]
+	maxBit := stringutils.Atoi(maxZ[1:])
+	carry, wrongs := check0Bit(gates)
+	for i := 1; i < maxBit; i++ {
+		var newWrongs []wire
+		carry, newWrongs = checkNthBit(gates, i, carry)
+		wrongs = append(wrongs, newWrongs...)
+	}
+	newWrongs := checkLastBit(maxBit, carry)
+	wrongs = append(wrongs, newWrongs...)
+	fmt.Println(wrongs)
 	return ""
 }
