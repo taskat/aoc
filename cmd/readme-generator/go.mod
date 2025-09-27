bc03ae8fd727d5ed1957cd547077cd1d76771c67
taskat <tamas.taska@gmx.com>
2025-09-27 17:34:43 +0000

    Add readne generator and workflow to auto-generate README.md

---
+++ cmd/readme-generator/go.mod
@@ -0,0 +1,7 @@
+module taskat/aoc/cmd/readme-generator
+
+go 1.25.0
+
+require github.com/taskat/aoc v0.0.0
+
+replace github.com/taskat/aoc => ../..
---
+++ cmd/readme-generator/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/taskat/aoc/pkg/common"
+)
+
+// templateValues represents the values used in the templates
+type templateValues struct {
+	Date string
+}
+
+// newTemplateValues creates a new template values
+func newTemplateValues() templateValues {
+	return templateValues{
+		Date: time.Now().Format("2006-01-02 15:04:05"),
+	}
+}
+
+// instantiateFile creates a file from a template file
+func instantiateFile(src, dest string, values templateValues) {
+	fmt.Println("Creating file", dest, "from", src)
+	dir := filepath.Dir(dest)
+	err := os.MkdirAll(dir, os.ModePerm)
+	common.QuitIfError(err, "Error creating directories:")
+	f, err := os.Create(dest)
+	common.QuitIfError(err, "Error creating file:")
+	defer f.Close()
+	if strings.HasSuffix(f.Name(), ".txt") {
+		return
+	}
+	err = os.Rename(f.Name(), strings.Replace(f.Name(), ".tmpl", "", 1))
+	common.QuitIfError(err, "Error renaming file:")
+	templateText, err := os.ReadFile(src)
+	common.QuitIfError(err, "Error reading template file:")
+	t := template.Must(template.New("file").Parse(string(templateText)))
+	err = t.Execute(f, values)
+	common.QuitIfError(err, "Error executing template:")
+}
+
+func main() {
+	fmt.Println("Generating README.md")
+	values := newTemplateValues()
+	instantiateFile("templates/readme.md.tmpl", "./generated/README.md", values)
+	fmt.Println("Done")
+}
