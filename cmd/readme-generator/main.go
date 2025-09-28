fb9973b9eabb612e7059dea8f74b1fe2f353b179
taskat <tamas.taska@gmx.com>
2025-09-28 11:35:03 +0000

    Add benchmark result handling

--- cmd/readme-generator/main.go
+++ cmd/readme-generator/main.go
@@ -13,13 +13,15 @@ import (
 
 // templateValues represents the values used in the templates
 type templateValues struct {
-	Date string
+	Date    string
+	Results FullResult
 }
 
 // newTemplateValues creates a new template values
-func newTemplateValues() templateValues {
+func newTemplateValues(results FullResult) templateValues {
 	return templateValues{
-		Date: time.Now().Format("2006-01-02 15:04:05"),
+		Date:    time.Now().Format("2006-01-02 15:04:05"),
+		Results: results,
 	}
 }
 
@@ -43,8 +45,15 @@ func instantiateFile(src, dest string, values templateValues) {
 
 func main() {
 	fmt.Println("Generating README.md")
-	values := newTemplateValues()
 	root := os.Getenv("AOC_HOME")
+	existingFile := filepath.Join(root, "benchmark.json")
+	existing := FromFile(existingFile)
+	fmt.Println("Parsed existing results")
+	existing.updateResults("/tmp/aoc/benchmark-results.txt")
+	fmt.Println("Updated results with new benchmark values")
+	existing.saveToFile(existingFile)
+	fmt.Println("Saved updated results to benchmark.json")
+	values := newTemplateValues(existing)
 	templatePath := filepath.Join(root, "cmd/readme-generator/templates/readme.md.tmpl")
 	destinationPath := filepath.Join(root, "README.md")
 	instantiateFile(templatePath, destinationPath, values)
