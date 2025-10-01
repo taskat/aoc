4e56c3ad6b4633bd48834d3f57523cf1c3ef4429
taskat <tamas.taska@gmx.com>
2025-10-01 20:57:34 +0000

    Fix package listing

--- internal/years/2024/01/solver.go
+++ internal/years/2024/01/solver.go
@@ -26,15 +26,15 @@ func (s *Solver) AddHyperParams(params ...string) {}
 
 // parse handle the common parsing logic for both parts
 func (s *Solver) parse(lines []string) ([]int, []int) {
-	left, right2 := make([]int, len(lines)), make([]int, len(lines))
+	left, right := make([]int, len(lines)), make([]int, len(lines))
 	for i, line := range lines {
 		parts := strings.Split(line, "   ")
 		left[i] = stringutils.Atoi(parts[0])
-		right2[i] = stringutils.Atoi(parts[1])
+		right[i] = stringutils.Atoi(parts[1])
 	}
 	sort.Ints(left)
-	sort.Ints(right2)
-	return left, right2
+	sort.Ints(right)
+	return left, right
 }
 
 // SolvePart1 solves part 1 of the puzzle
