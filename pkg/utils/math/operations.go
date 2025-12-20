9c46985309e4fddc827b561e6fa4068b5203b1a0
taskat <tamas.taska@gmx.com>
2025-12-20 17:07:34 +0000

    Add rounding methods to math package

--- pkg/utils/math/operations.go
+++ pkg/utils/math/operations.go
@@ -9,3 +9,29 @@ func Abs[T types.Real](number T) T {
 	}
 	return number
 }
+
+// Ceil returns the smallest integer value greater than or equal to the given number
+func Ceil[T types.Real](number T) int {
+	i := int(number)
+	if T(i) < number {
+		return i + 1
+	}
+	return i
+}
+
+// Floor returns the largest integer value less than or equal to the given number
+func Floor[T types.Real](number T) int {
+	i := int(number)
+	if T(i) > number {
+		return i - 1
+	}
+	return i
+}
+
+// Round returns the nearest integer to the given number
+func Round[T types.Real](number T) int {
+	if number > 0 {
+		return int(Floor(float64(number) + 0.5))
+	}
+	return int(Ceil(float64(number) - 0.5))
+}
--- pkg/utils/math/operations_test.go
+++ pkg/utils/math/operations_test.go
@@ -23,3 +23,73 @@ func TestAbs(t *testing.T) {
 		})
 	}
 }
+
+func TestCeil(t *testing.T) {
+	tests := []struct {
+		name     string
+		number   float64
+		expected int
+	}{
+		{"Integer", 2.0, 2},
+		{"Fractional", 2.3, 3},
+		{"Negative Fractional", -2.3, -2},
+		{"Negative Integer", -2.0, -2},
+		{"Zero", 0.0, 0},
+		{"Small positive", 0.000000000000001, 1},
+		{"Large positive", 0.999999999999999, 1},
+		{"Small negative", -0.000000000000001, 0},
+		{"Large negative", -0.999999999999999, 0},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := Ceil(tc.number)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestFloor(t *testing.T) {
+	tests := []struct {
+		name     string
+		number   float64
+		expected int
+	}{
+		{"Integer", 2.0, 2},
+		{"Fractional", 2.7, 2},
+		{"Negative Fractional", -2.3, -3},
+		{"Negative Integer", -2.0, -2},
+		{"Zero", 0.0, 0},
+		{"Small positive", 0.000000000000001, 0},
+		{"Large positive", 0.999999999999999, 0},
+		{"Small negative", -0.000000000000001, -1},
+		{"Large negative", -0.999999999999999, -1},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := Floor(tc.number)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestRound(t *testing.T) {
+	tests := []struct {
+		name     string
+		number   float64
+		expected int
+	}{
+		{"Positive Half", 2.5, 3},
+		{"Positive Below Half", 2.4, 2},
+		{"Positive Above Half", 2.6, 3},
+		{"Negative Half", -2.5, -3},
+		{"Negative Below Half", -2.6, -3},
+		{"Negative Above Half", -2.4, -2},
+		{"Zero", 0.0, 0},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := Round(tc.number)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
